3fb150ad2a8636eae2c3327ca0009e03826331ea
fitosmon <fitosmon2@hotmail.com>
2022-10-03 14:35:34 -0500

    resolver primer ejercicio

--- tecweb/practicas/p04/src/funciones.php
+++ tecweb/practicas/p04/src/funciones.php
@@ -1,8 +1,13 @@
 <?php
 
 function multiplo5y7($numero) {
-    // AQUÍ VA EL CÓDIGO DE LA FUNCIÓN
-    return 'respuesta: '.$numero;
+    if ($numero%5==0 && $numero%7==0){
+        return 'respuesta: '.$numero.' es multiplo de 5 y 7';
+    }
+    else{
+        return 'respuesta: '.$numero.' no es multiplo de 5 y 7';
+    }
+        
 }
 
 ?>
